9dc07f79d111c317a3bd66785fccf4e6177097ee
Artyom Hovsepyan <artyomhovsepyan135@gmail.com>
2024-09-27 17:07:04 +0400

    logger && config init

---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"log/slog"
+	"os"
+	"sso/cmd/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.Any("config", cfg))
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+
+	return log
+}
--- sso/go.mod
+++ sso/go.mod
@@ -4,4 +4,11 @@ go 1.23.0
 
 replace github.com/tyomll/sso-go/protos => ../protos
 
-require github.com/tyomll/sso-go/protos v0.0.0-20240927115044-42db89456f0e // indirect
+require github.com/ilyakaznacheev/cleanenv v1.5.0
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
